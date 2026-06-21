208e3019732a9d6798e056814546b85d75abc59a
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-06-21 11:39:25 -0700

    Final API working version

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import torch
-from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
+# import torch
+# from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
 import json
 import ast
 from PIL import Image, ImageDraw, ImageFont
@@ -8,17 +8,20 @@ import io
 import base64
 import os
 from openai import OpenAI
+from dotenv import load_dotenv
 
-st.set_page_config(page_title="Qwen 2.5 OCR", layout="wide")
+load_dotenv()
+
+st.set_page_config(page_title="Qwen 3 OCR", layout="wide")
 
 # Sidebar for image upload
-st.sidebar.title("Qwen 2.5 OCR")
+st.sidebar.title("Qwen 3 OCR")
 uploaded_file = st.sidebar.file_uploader("Upload an iamge", type = ["jpg", "jpeg", "png"])
 
 # Display the uploaded image in sidebar
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.sidebar.image(image, caption="Uploaded Image", use_column_width=True)
+    st.sidebar.image(image, caption="Uploaded Image", width="content")
 
     # Save the uploaded image temporarily
     temp_image_path = "temp_uploaded_image.jpg"
@@ -40,38 +43,38 @@ def parse_json(json_output):
             break # exit the loop once ```json is found
     return json_output
 
-def inference(image_path, prompt, sys_prompt="You are a helpful assistant.", max_new_tokens=4096, return_input=False):
-    image = Image.open(image_path)
-    image_local_path = "file://" + image_path
-    messages = [
-        {"role": "system", "content": sys_prompt},
-        {"role": "user", "content": [
-            {"type" : "text", "text": prompt},
-            {"image": image_local_path}
-        ]}
-    ]
+# def inference(image_path, prompt, sys_prompt="You are a helpful assistant.", max_new_tokens=4096, return_input=False):
+#     image = Image.open(image_path)
+#     image_local_path = "file://" + image_path
+#     messages = [
+#         {"role": "system", "content": sys_prompt},
+#         {"role": "user", "content": [
+#             {"type" : "text", "text": prompt},
+#             {"image": image_local_path}
+#         ]}
+#     ]
 
-    text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+#     text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 
-    inputs = processor(text=[text], images=[image], padding=True, return_tensors="pt")
-    inputs = inputs.to(device)
+#     inputs = processor(text=[text], images=[image], padding=True, return_tensors="pt")
+#     inputs = inputs.to(device)
 
-    output_ids = model.generate(**inputs, max_new_tokens=max_new_tokens)
-    generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(inputs.input_ids, output_ids)]
-    output_text = processor.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+#     output_ids = model.generate(**inputs, max_new_tokens=max_new_tokens)
+#     generated_ids = [output_ids[len(input_ids):] for input_ids, output_ids in zip(inputs.input_ids, output_ids)]
+#     output_text = processor.batch_decode(generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True)
 
-    if return_input:
-        return output_text[0], inputs
-    else: 
-        return output_text[0]
+#     if return_input:
+#         return output_text[0], inputs
+#     else: 
+#         return output_text[0]
     
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
     
-def inference_with_api(image_path, prompt, sys_prompt="You are a helpful assistant.", model_id="qwen2.5-vl-72b-instruct", min_pixels=512*28*28, max_pixels=2048*28*28):
+def inference_with_api(image_path, prompt, sys_prompt="You are a helpful assistant.", model_id="qwen/qwen3-vl-30b-a3b-instruct", min_pixels=512*32*32, max_pixels=2048*32*32):
     base64_image = encode_image(image_path)
-    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY")) # !!!!! configure this
+    client = OpenAI(api_key=os.getenv("OPENROUTER_API_KEY"), base_url="https://openrouter.ai/api/v1") 
     
     messages = [
         {
@@ -84,7 +87,8 @@ def inference_with_api(image_path, prompt, sys_prompt="You are a helpful assista
                 {
                     "type": "image_url",
                     "min_pixels": min_pixels,
-                    "max_pixels" : max_pixels
+                    "max_pixels" : max_pixels,
+                    "image_url":{"url" : f"data:image/jpeg;base64,{base64_image}"}
                 },
                 {
                     "type": "text",
@@ -101,7 +105,7 @@ def inference_with_api(image_path, prompt, sys_prompt="You are a helpful assista
     return completion.choices[0].message.content
 
 
-def plot_text_bounding_boxes(image_path, bounding_boxes, input_width, input_height):
+def plot_text_bounding_boxes(image_path, bounding_boxes):
     """
     Plots bounding boxes on an image with markers for each a name, using PIL, normalized coordinates, and different colors.
     """
@@ -126,10 +130,10 @@ def plot_text_bounding_boxes(image_path, bounding_boxes, input_width, input_heig
         color = 'green'
         
         # Convert normalized coordinates to absolute coordinates
-        abs_y1 = int(bounding_box["bbox_2d"][1]/input_height*height)
-        abs_x1 = int(bounding_box["bbox_2d"][0]/input_width*width)
-        abs_y2 = int(bounding_box["bbox_2d"][3]/input_height*height)
-        abs_x2 = int(bounding_box["bbox_2d"][2]/input_width*width)
+        abs_y1 = int(bounding_box["bbox_2d"][1]/999*height)
+        abs_x1 = int(bounding_box["bbox_2d"][0]/999*width)
+        abs_y2 = int(bounding_box["bbox_2d"][3]/999*height)
+        abs_x2 = int(bounding_box["bbox_2d"][2]/999*width)
         
         if abs_x1 > abs_x2:
             abs_x1, abs_x2 = abs_x2, abs_x1
@@ -145,12 +149,13 @@ def plot_text_bounding_boxes(image_path, bounding_boxes, input_width, input_heig
         # Draw the text
         if "text_content" in bounding_box:
             draw.text((abs_x1, abs_y2), bounding_box["text_content"], fill=color, font=font)
-            
-        return img
+    
+
+    return {"img": img, "content": ast.literal_eval(bounding_boxes)}
     
     
 # Main content
-st.title("Qwen 2.5 OCR")
+st.title("Qwen 3 OCR")
 
 # Mode selection
 mode = st.radio("Select mode:", ["Full Page OCR", "Text Spotting"])
@@ -158,23 +163,23 @@ mode = st.radio("Select mode:", ["Full Page OCR", "Text Spotting"])
 
 # Load Model
 
-@st.cache_resource
-def load_model():
-    checkpoint = "Qwen/Qwen2.5-VL-7B-Instruct"
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    checkpoint,
-    torch_dtype=torch.bfloat16 if torch.cuda.is_available() else torch.float32,
-    attn_implementation="flash_attention_2" if torch.cuda.is_available() else "eager",
-    device_map="auto" if torch.cuda.is_available() else None
-    )
-    processor = AutoProcessor.from_pretrained(checkpoint)
-    return model, processor, device
+# @st.cache_resource
+# def load_model():
+#     checkpoint = "Qwen/Qwen2.5-VL-7B-Instruct"
+#     device = "cuda" if torch.cuda.is_available() else "cpu"
+#     model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+#     checkpoint,
+#     torch_dtype=torch.bfloat16 if torch.cuda.is_available() else torch.float32,
+#     attn_implementation="flash_attention_2" if torch.cuda.is_available() else "eager",
+#     device_map="auto" if torch.cuda.is_available() else None
+#     )
+#     processor = AutoProcessor.from_pretrained(checkpoint)
+#     return model, processor, device
     
     
 # Init model
-with st.spinner("Loading Model...."):
-    model, processor, device = load_model()
+# with st.spinner("Loading Model...."):
+#     model, processor, device = load_model()
     
 if uploaded_file is not None:
     if mode == "Full Page OCR":
@@ -182,40 +187,37 @@ if uploaded_file is not None:
         
         if st.button("Extract Text"):
             with st.spinner("Extracting text..."):
-                prompt = "Please output only the text content from the iamge without any additional descriptions or formatting."
+                prompt = "Please output only the text content from the image without any additional descriptions or formatting."
                 try:
-                    response = inference(temp_image_path, prompt)
+                    response = inference_with_api(temp_image_path, prompt)
+                    st.markdown(response)
                 except Exception as e:
                     st.error(f"Error during inference: {str(e)}")
-        elif mode == "Text Spotting":
-            st.header("Text Spotting")
+    elif mode == "Text Spotting":
+        st.header("Text Spotting")
+        
+        if st.button("Spot Text"):
+            with st.spinner("Spotting text..."):
+                prompt = "Spotting all the text in the image with line-level, and output in JSON format."
+                
+                try:
+                    response = inference_with_api(temp_image_path, prompt)
+                    # Create image with bounding boxes
+                    result = plot_text_bounding_boxes(temp_image_path, response)
             
-            if st.button("Spot Text"):
-                with st.spinner("Spotting text..."):
-                    prompt = "Spotting all the text in the image with line-level, and output in JSON format."
+                    # Display results
+                    col1, col2 = st.columns(2)
                     
-                    try:
-                        response, inputs = inference(temp_image_path, prompt, return_input=True)
-                        
-                        # Get input dimensions
-                        input_height = inputs['image_grid_thw'][0][1]*14
-                        input_width = inputs['image_grid_thw'][0][2]*14
-                        
-                        # Create image with bounding boxes
-                        result_image = plot_text_bounding_boxes(temp_image_path, response, input_width, input_height)
-                        
-                        # Display results
-                        col1, col2 = st.columns(2)
-                        
-                        with col1:
-                            st.image(result_image, caption="Text Spotting Result", use_column_width=True)
-                        with col2:
-                            st.markdown("### Detected Text:")
+                    with col1:
+                        st.image(result["img"], caption="Text Spotting Result", width="content")
+                    with col2:
+                        st.markdown("### Detected Text:")
+                        for r in result["content"]:
+                            st.markdown(r["text_content"])
                             
-                            st.markdown(response)
-                            
-                    except Exception as e:
-                        st.error(f"Error during inference: {str(e)}")
+                        
+                except Exception as e:
+                    st.error(f"Error during inference: {str(e)}")
 
 else:
     st.info("Please upload an image to being.")
